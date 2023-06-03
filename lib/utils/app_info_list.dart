03d24e817b57853dd9e33a9e8c94a172a7f81171
Sibirajla <115621077+Sibirajla@users.noreply.github.com>
2023-06-03 11:47:47 +0530

    Add files via upload

---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,65 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'one.png',
+    'place': 'Open Space',
+    'destination': 'London',
+    'price': 25
+  },
+  {
+    'image': 'two.png',
+    'place': 'Global Will',
+    'destination': 'London',
+    'price': 40
+  },
+  {
+    'image': 'three.png',
+    'place': 'Tallest Building',
+    'destination': 'Dubai',
+    'price': 68
+  },
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {
+      'code':"NYC",
+      'name':"New-York"
+    },
+    'to': {
+      'code':"LDN",
+      'name':"London"
+    },
+    'flying_time': '8H 30M',
+    'date': "1 MAY",
+    'departure_time':"08:00 AM",
+    "number":23
+  },
+  {
+    'from': {
+      'code':"DK",
+      'name':"Dhaka"
+    },
+    'to': {
+      'code':"SH",
+      'name':"Shanghai"
+    },
+    'flying_time': '4H 20M',
+    'date': "10 MAY",
+    'departure_time':"09:00 AM",
+    "number":45
+  },
+  {
+    'from': {
+      'code':"IN",
+      'name':"India"
+    },
+    'to': {
+      'code':"SG",
+      'name':"Singapore"
+    },
+    'flying_time': '5H 10M',
+    'date': "23 JUL",
+    'departure_time':"08:00 PM",
+    "number":36
+  }
+];
---
+++ lib/widgets/layout_builder_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppLayoutBuilderWidget extends StatelessWidget {
+  final bool? isColor;
+  final int sections;
+  final double width;
+  const AppLayoutBuilderWidget({Key? key, this.isColor, required this.sections, this.width=3}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+
+      builder: (BuildContext context , BoxConstraints constrains ) {
+        print("The width is ${constrains.constrainWidth()}");
+        return Flex(
+          direction: Axis.horizontal,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          children: List.generate((constrains.constrainWidth()/sections).floor(), (index) =>  SizedBox(
+            width: width,height: 1,child: DecoratedBox(decoration: BoxDecoration(
+              color: isColor==null?Colors.white:Colors.grey.shade300
+          ),),
+          )),
+        );
+      },
+
+    );
+  }
+}
